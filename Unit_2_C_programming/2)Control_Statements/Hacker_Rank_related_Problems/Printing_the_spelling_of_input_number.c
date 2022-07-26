e42d684973373dfca2c02eb1691a3a6e53966edb
maiabozaid <77507104+maiabozaid@users.noreply.github.com>
2022-07-27 01:12:10 +0200

    Create Printing_the_spelling_of_input_number.c

---
+++ Unit_2_C_programming/2)Control_Statements/Hacker_Rank_related_Problems/Printing_the_spelling_of_input_number.c
@@ -0,0 +1,34 @@
+/* Problem Link on Hacker rank: https://www.hackerrank.com/challenges/conditional-statements-in-c/problem?isFullScreen=false */
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+
+
+int main()
+{
+    int n=0;
+    scanf("%d",&n);
+    if(n==1) printf("one");
+    else if(n==2) printf("two");
+    else if(n==3) printf("three");
+    else if(n==4) printf("four");
+    else if(n==5) printf("five");
+    else if(n==6) printf("six");
+    else if(n==7) printf("seven");
+    else if(n==8) printf("eight");
+    else if(n==9) printf("nine");
+    else if(n>9) printf("Greater than 9");
+    else printf("kindlly enter a number = 1 or greater");
+
+    return 0;
+}
